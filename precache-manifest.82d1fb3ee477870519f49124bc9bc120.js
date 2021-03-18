44e94479ac1cfbc4621414a13e951f532e6dd024
Julio Antony <julioantony2@gmail.com>
2021-03-18 14:43:15 +0700

    Menyalin isi folder build

---
+++ precache-manifest.82d1fb3ee477870519f49124bc9bc120.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61e5e640f9d07bc98457",
+    "url": "/static/css/main.af43c806.chunk.css"
+  },
+  {
+    "revision": "61e5e640f9d07bc98457",
+    "url": "/static/js/main.61e5e640.chunk.js"
+  },
+  {
+    "revision": "dcb4138fdc682d52f680",
+    "url": "/static/js/1.dcb4138f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf13018bbbe6aed9d4df2dcfc5d4e9a1",
+    "url": "/index.html"
+  }
+];
